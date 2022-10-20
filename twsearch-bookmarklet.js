4cc99eca539034f7defb971cd272bd54d38c677e
taka <37933370+takatakasuppon@users.noreply.github.com>
2022-10-20 20:16:12 +0900

    Delete twsearch-bookmarklet.js

--- twsearch-bookmarklet.js
+++
@@ -1,1 +0,0 @@
-javascript: (function () {  let color = prompt('Background Color?%20Hex%20or%20name%20will%20do%27,%20%27pink%27);%20%20document.body.style.backgroundColor%20=%20color;%20%20let%20divElements%20=%20document.querySelectorAll(%27div%27);%20%20for%20(elements%20of%20divElements)%20{%20%20%20elements.style.backgroundColor%20=%20color;%20%20}})();
